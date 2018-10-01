2c5e92a3304e322e225312e4eb746c2029f0d2d1
U9601 <ctjones1965@gmail.com>
2018-10-01 19:12:51 +0100

    Fixing CSS

--- home.py
+++ home.py
@@ -3,9 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-        start = '<"'
-        url = url_for('static', filename='style.css')
-        end = '">'
         return render_template('home.html')
 
 if __name__ == "__main__":
